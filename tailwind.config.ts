c355e78ce9b8401de22e2cf46562a607aef5a31e
Julia Undeutsch <JUndeutsch@gmx.at>
2023-10-29 15:34:43 +0100

    bug

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 const config: Config = {
   content: [
@@ -12,6 +13,16 @@ const config: Config = {
         'accent-1': '#FAFAFA',
         'accent-2': '#EAEAEA',
         'accent-7': '#333',
+        'blue-200': '#e6effe',
+        'blue-800': '#084FD4',
+        'purple-200': '#f0e5fb',
+        'purple-800': '#790BE0',
+        'green-200': '#1E6B65',
+        'green-800': '#ebfaf8',
+        'red-200': '#fce8f1',
+        'red-800': '#D11B67',
+        'yellow-200': '#fffbe6',
+        'yellow-800': '#ffd700',
         success: '#0070f3',
         cyan: '#79FFE1',
       },
@@ -21,9 +32,9 @@ const config: Config = {
       letterSpacing: {
         tighter: '-.04em',
       },
-      lineHeight: {
-        tight: 1.2,
-      },
+      // lineHeight: {
+      //   tight: 1.2,
+      // },
       fontSize: {
         '5xl': '2.5rem',
         '6xl': '2.75rem',
@@ -33,6 +44,10 @@ const config: Config = {
       boxShadow: {
         sm: '0 5px 10px rgba(0, 0, 0, 0.12)',
         md: '0 8px 30px rgba(0, 0, 0, 0.12)',
+      },
+      screens: {
+        'xs': '475px',
+        ...defaultTheme.screens,
       },
     },
   },
