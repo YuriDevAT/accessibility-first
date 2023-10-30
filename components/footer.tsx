68192c009b6348e30a4593f4d57390b8efd20f06
Julia Undeutsch <JUndeutsch@gmx.at>
2023-10-30 08:52:43 +0100

    add link; adjust font size and decoration

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,19 +3,19 @@ import { CMS_NAME } from '../lib/constants'
 
 const Footer = () => {
   return (
-    <footer className="bg-neutral-50 border-t border-neutral-200">
+    <footer className="bg-neutral-50 border-t border-neutral-200 text-lg">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h2 className="sr-only">Footer</h2>
           <div className="xl:gap-8 xl:grid-cols-4 grid sm:grid-cols-2 gap-y-4 md:grid-cols-4 mb-4">
-            <a href="" className="md:justify-self-end">
+            <a href="/" className="md:justify-self-end">
               <span className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2 hover:underline">
                 Accessibility First
               </span>
             </a>
             <div className="xs:ml-4 sm:mx-auto">
               <h3 className="font-semibold m-0 uppercase">Blog</h3>
-              <ul className="mt-2">
+              <ul className="mt-2 underline">
                 <li>
                   <a href="articles" className="block py-1">
                     All articles
@@ -45,7 +45,7 @@ const Footer = () => {
             </div>
             <div className="xs:ml-4 sm:mx-auto">
               <h3 className="font-semibold m-0 uppercase">Organization</h3>
-              <ul className="mt-2">
+              <ul className="mt-2 underline">
                 <li>
                   <a href="about" className="block py-1">
                     About
@@ -80,19 +80,19 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="sm:mt-6 border-t border-t-1px border-slate-300 py-4 text-center text-lg ">
+        <div className="sm:mt-6 border-t border-t-1px border-slate-300 py-4 text-center">
           <p className="my-5 md:pl-8">
             A statically generated blog using{' '}
             <a
               href="https://nextjs.org/"
-              className="underline hover:text-blue-600 duration-200 transition-colors"
+              className="underline hover:text-blue-800 duration-200 transition-colors"
             >
               Next.js
             </a>{' '}
             and {CMS_NAME}.
           </p>
-          <p className="text-slate-800">
-            Made with <span className="text-red-800">❤</span> and ♫ | Copyright © Accessibility First 2022-{new Date().getFullYear()}. All rights reserved.
+          <p>
+            Made with <span className="text-red-800">&#9829;</span> and ♫ | Copyright © Accessibility First 2022-{new Date().getFullYear()}. All rights reserved.
           </p>
         </div>
       </Container>
