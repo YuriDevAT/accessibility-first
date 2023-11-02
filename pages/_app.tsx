5276e2544380f810c1b0bb5fa275bdaa9f680de7
Julia Undeutsch <JUndeutsch@gmx.at>
2023-11-02 10:03:22 +0100

    add custom font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
+import localFont from '@next/font/local'
+
+const alliance = localFont({
+  src: [
+    {
+      path: '../public/fonts/Alliance-Light.otf',
+      weight: '400'
+    }
+  ],
+  variable: '--font-alliance'
+})
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${alliance.variable} font-sans`}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
