a294c5c5daf1f633a9e759f739831fcd275d29d9
Julia Undeutsch <JUndeutsch@gmx.at>
2023-11-04 18:55:12 +0100

    update empty files links coming soon

---
+++ pages/web-development.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Layout from '../components/layout'
+import Container from '../components/container'
+
+const WebDevelopment = () => {
+  return (
+    <Layout>
+      <Container>
+        <section className="mt-16 mb-32">
+          <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">Coming soon</h1>
+          <Image src={"/assets/coming-soon.png"} width={250} height={250} alt="" />
+        </section>
+      </Container>
+    </Layout >
+  )
+}
+
+export default WebDevelopment
