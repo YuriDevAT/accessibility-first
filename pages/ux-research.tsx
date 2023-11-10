f9c517e7110a95bce773197a355cb8931ec1e615
Julia Undeutsch <JUndeutsch@gmx.at>
2023-11-10 16:56:40 +0100

    update dynamic content for posts

--- pages/ux-research.tsx
+++ pages/ux-research.tsx
@@ -2,20 +2,33 @@ import Image from "next/image";
 import Layout from '../components/layout'
 import Container from '../components/container'
 import Head from "next/head";
+import PostGrid from '../components/post-grid'
+import { getAllPosts } from '../lib/api'
+import Post from '../interfaces/post'
 
 const title = "UX Research"
 
-const UXResearch = () => {
+type Props = {
+  allPosts: Post[]
+}
+
+const UXResearch = ({ allPosts }: Props) => {
+  const posts = allPosts.filter((category) => category.category === "UX Research")
+
   return (
     <Layout>
       <Head>
         <title>{title}</title>
       </Head>
       <Container>
+        <h1 className="mt-16 text-5xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">{title}</h1>
         <section className="mt-16 mb-32">
-          <h1 className="text-5xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">{title}</h1>
-          <p className="text-3xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">Coming soon</p>
-          <Image src={"/assets/coming-soon.png"} width={250} height={250} alt="" />
+          {posts.length > 0 ? <PostGrid posts={posts} /> :
+            <>
+              <p className="text-3xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">Nothing there yet</p>
+              <Image src={"/assets/coming-soon.png"} width={250} height={250} alt="" />
+            </>
+          }
         </section>
       </Container>
     </Layout >
@@ -23,3 +36,19 @@ const UXResearch = () => {
 }
 
 export default UXResearch
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+    'category'
+  ])
+
+  return {
+    props: { allPosts },
+  }
+}
