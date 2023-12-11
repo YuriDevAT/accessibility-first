eab1ada4a75adcc192710cc2c3cd5b865f916520
Julia Undeutsch <JUndeutsch@gmx.at>
2023-12-11 10:30:58 +0100

    add skip link for theme

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,7 @@
 import Alert from './alert'
 import Footer from './footer'
 import Meta from './meta'
+import SkipLink from '../components/skip-link'
 
 type Props = {
   preview?: boolean
@@ -11,6 +12,7 @@ const Layout = ({ preview, children }: Props) => {
   return (
     <>
       <Meta />
+      <SkipLink />  
       <div className="min-h-screen">
         {/* <Alert preview={preview} /> */}
         <main>{children}</main>
---
+++ components/skip-link.tsx
@@ -0,0 +1,5 @@
+const SkipLink = () => {
+  return <a href="#theme-btn" className="sr-only focus:not-sr-only">Skip to theme button</a>
+}
+
+export default SkipLink
