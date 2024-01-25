b9697a2c3c4a5494730b03e002fc9ec154a8794f
YuriDevAT <JUndeutsch@gmx.at>
2024-01-25 14:01:05 +0100

    adjust text color sub

--- pages/events.tsx
+++ pages/events.tsx
@@ -19,11 +19,11 @@ const Events = () => {
       </Head>
       <Container>
         <h1 className="mt-16 text-5xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">{title}</h1>
-        <section className="mt-16 mb-32 max-w-4xl dark:text-slate-800">
+        <section className="mt-16 mb-32 max-w-4xl">
           <p className="text-xl leading-relaxed my-6">We are available for motivational and informative speaking. We love to write about your event, create and share content on all major platforms like LinkedIn, Instagram, Twitter and more.</p>
           <p className="text-xl leading-relaxed my-6">You&apos;re also likely to find us attending local accessibility meetings, as well as local and international tech conferences.</p>
           <h2 className='text-3xl mt-12 mb-4 leading-snug font-bold text-center md:text-left'>Upcoming Events</h2>
-          <ul className="text-lg leading-relaxed my-6">
+          <ul className="text-lg leading-relaxed my-6 dark:text-slate-800">
             {upcomingEvents.map((event, index) => (
               <li key={event.date} className={`${index % 2 !== 0 ? 'bg-yellow-200 dark:bg-yellow-500' : event.speaker === 'Julia' ? 'bg-blue-200 dark:bg-blue-500' : 'bg-purple-200 dark:bg-purple-500'
               } py-1 px-4 rounded-md my-3`}>
@@ -33,7 +33,7 @@ const Events = () => {
             ))}
           </ul>
           <h2 className='text-3xl mt-12 mb-4 leading-snug font-bold text-center md:text-left'>Past Events</h2>
-          <ul>
+          <ul className="dark:text-slate-800">
             {pastEvents.map((event, index) => (
               <li key={event.date} className={`${index % 2 !== 0 ? 'bg-yellow-200 dark:bg-yellow-500' : event.speaker === 'Julia' ? 'bg-blue-200 dark:bg-blue-500' : 'bg-purple-200 dark:bg-purple-500'
             } py-1 px-4 rounded-md my-3`}>
