56c2d0346bb0bf047535ae2e97794765d15842f7
YuriDevAT <JUndeutsch@gmx.at>
2024-05-21 20:11:14 +0900

    add focus;

--- components/post-original.tsx
+++ components/post-original.tsx
@@ -10,7 +10,7 @@ const PostOriginal = ({ ogPost }: Props) => {
   return (
     <p className="text-lg italic tracking-tighter leading-tight mb-12 text-right">
       This article was originally published at{' '}
-      <a className="underline dark:text-slate-200" href={ogPost}>
+      <a className="underline dark:text-slate-200 outline-none focus:ring focus:ring-slate-950 dark:focus:ring-slate-200" href={ogPost}>
         {isJulia ? 'dev.to/yuridevat' : 'laura-wissiak.com/blog'}
         </a>
     </p>
