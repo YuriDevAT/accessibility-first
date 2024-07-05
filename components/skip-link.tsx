6728c213fae2f864cb11357a1013ee04065343f8
YuriDevAT <JUndeutsch@gmx.at>
2024-07-05 17:51:54 +0200

    add landmark to skip;

--- components/skip-link.tsx
+++ components/skip-link.tsx
@@ -4,27 +4,30 @@ const SkipLink = () => {
   const { t } = useTranslation('skip-link');
 
   return (
-    <ul className="focus-within:border-slate-950">
-      <li>
-        <a
-          href="#theme-btn"
-          className="sr-only focus:not-sr-only"
-        >
-          {t('a1')}
-        </a>
-      </li>
-      <li>
-        <a href="#reportBug-btn" className="sr-only focus:not-sr-only">
-          {t('a2')}
-        </a>
-      </li>
-      <li>
-        {' '}
-        <a href="#i18n-btn" className="sr-only focus:not-sr-only">
-          {t('a3')}
-        </a>
-      </li>
-    </ul>
+    <nav aria-labelledby="skiplinks">
+      <p id="skiplinks" className="sr-only">Skiplinks</p>
+      <ul className="focus-within:border-slate-950">
+        <li>
+          <a
+            href="#theme-btn"
+            className="sr-only focus:not-sr-only"
+          >
+            {t('a1')}
+          </a>
+        </li>
+        <li>
+          <a href="#reportBug-btn" className="sr-only focus:not-sr-only">
+            {t('a2')}
+          </a>
+        </li>
+        <li>
+          {' '}
+          <a href="#i18n-btn" className="sr-only focus:not-sr-only">
+            {t('a3')}
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
