7973bf2005c16d84b6007ce3a784dec00b6c3188
YuriDevAT <JUndeutsch@gmx.at>
2025-01-11 17:17:00 +0100

    Update: newsletter text i18n;

--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -1,18 +1,29 @@
-import Link from "next/link";
+import Link from 'next/link';
+import useTranslation from 'next-translate/useTranslation';
 
 const Newsletter = () => {
+  const { t } = useTranslation('newsletter');
 
   return (
     <section className="mb-32 sm:p-12 text-center">
       <div className="border lg:w-4/5 px-3 py-6 md:p-16 mx-auto h-auto">
-        <h2 className="text-4xl mb-4">Subscribe to A11y News newsletter!</h2>
-        <p className="text-lg mb-8">Never miss an event again by subscribing to Laura's newsletter A11y News. <br />Events worldwide, in Austria, in person or online, in English or German - Laura, keeps you in the loop.</p>
+        <h2 className="text-4xl mb-4">{t('h')}</h2>
+        <p className="text-lg mb-8">
+          {t('p-1')}
+          <br />
+          {t('p-2')}
+        </p>
         <div className="p-4">
-          <Link href="https://a11ynews.substack.com/" className="bg-purple-500 p-3 inline-block focus:outline dark:outline-white outline-slate-950 outline-offset-1 text-slate-950">Subscribe to A11y News Newsletter on Substack</Link>
+          <Link
+            href="https://a11ynews.substack.com/"
+            className="bg-purple-500 p-3 inline-block focus:outline dark:outline-white outline-slate-950 outline-offset-1 text-slate-950"
+          >
+            {t('a')}
+          </Link>
         </div>
       </div>
     </section>
-  )
+  );
 };
 
 export default Newsletter;
--- i18n.js
+++ i18n.js
@@ -19,6 +19,7 @@ module.exports = {
       'reading-time',
       'category',
       'video',
+      'newsletter',
     ],
     '/all-posts': ['all-posts'],
     '/about': ['about', 'team'],
